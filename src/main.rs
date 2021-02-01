229a4a3aea01e1f786e8862a2a1d00a1e1952b99
Ivan Tham <pickfire@riseup.net>
2021-02-02 01:37:30 +0800

    Refactor table dsl imports

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use diesel::prelude::*;
 
 fn main() {
-    use hello::schema::comments::dsl::*;
-    use hello::schema::posts::dsl::*;
-    use hello::schema::users::dsl::*;
-    use hello::schema::comments;
+    use hello::schema::{comments, posts, users};
 
     let conn = hello::establish_connection();
 
-    let query = comments
-        .inner_join(posts)
-        .inner_join(users)
-        .select((comments::columns::body, title, name))
-        .filter(name.eq("John"));
+    let query = comments::table
+        .inner_join(posts::table)
+        .inner_join(users::table)
+        .select((comments::body, posts::title, users::name))
+        .filter(users::name.eq("John"));
     println!(
         "{}",
         diesel::debug_query::<diesel::sqlite::Sqlite, _>(&query).to_string()
--- src/schema.rs
+++ src/schema.rs
@@ -23,8 +23,4 @@ table! {
     }
 }
 
-allow_tables_to_appear_in_same_query!(
-    comments,
-    posts,
-    users,
-);
+allow_tables_to_appear_in_same_query!(comments, posts, users,);
